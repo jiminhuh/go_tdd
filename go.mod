6e2e673ec324a32aa5c7ffede869e95f8342ef01
Jimin Huh <jiminhuh95@gmail.com>
2021-05-22 21:37:38 -0500

    refactor and test for spanish and french

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module jiminhuh.com/m/v2
 
 go 1.16
 
-require golang.org/x/tools v0.1.1 // indirect
--- hello.go
+++ hello.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHellowPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	// if language == spanish {
+	// 	return spanishHelloPrefix + name
+	// }
+
+	// if language == french {
+	// 	return frenchHellowPrefix + name
+	// }
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHellowPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("Chris", "Spanish"))
 }
